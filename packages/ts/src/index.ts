70c1e6c1bdb33a7656401ca8a96e70fbcabfd941
kayhhh <basedlainpilled@gmail.com>
2023-07-31 14:57:52 -0400

    add message proto + improve ts package build

---
+++ packages/ts/src/index.ts
@@ -0,0 +1,6 @@
+export * from "./generated/content";
+export * from "./generated/websocket/message";
+export * from "./generated/websocket/world/request";
+export * from "./generated/websocket/world/response";
+export * from "./generated/websocket/webrtc/request";
+export * from "./generated/websocket/webrtc/response";
